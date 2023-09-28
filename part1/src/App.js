3f33535184b63bcd4cddb48a52992ab1c643a3fc
laubenitez79 <laubenitez@uade.edu.ar>
2023-09-28 12:50:29 -0300

    Nuevo commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <Mensaje color='red' message='Trabajando para mejorar'></Mensaje>
-      <Mensaje color='blue' message='Y'></Mensaje>
-      <Mensaje color='green' message='Poder tener mi propia web'></Mensaje>
+      <Mensaje color='blue' message='Y poder tener mi propia web'></Mensaje>
+      <Mensaje color='green' message='Para vender mis propios servicios'></Mensaje>
 
     </div>
   );
